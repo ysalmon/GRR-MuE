04f0480840a7943d374f04fc6440f42128306c46
Christian <christian.daviau@recia.fr>
2017-04-12 17:50:32 +0200

    Correction bug de référencement des include

--- changeEtablissement.php
+++ changeEtablissement.php
@@ -1,24 +1,23 @@
 <?php
 
-include_once "/include/connect.inc.php";
-include_once "/include/config.inc.php";
-include_once "/include/misc.inc.php";
-include_once "/include/mrbs_sql.inc.php";
-include_once "/include/functions.inc.php";
-include_once "/include/$dbsys.inc.php";
-include_once "/include/mincals.inc.php";
+include_once "include/connect.inc.php";
+include_once "include/config.inc.php";
+include_once "include/misc.inc.php";
+include_once "include/mrbs_sql.inc.php";
+include_once "include/functions.inc.php";
+include_once "include/$dbsys.inc.php";
+include_once "include/mincals.inc.php";
 $grr_script_name = "day.php";
 #Paramètres de connection
-require_once("/include/settings.class.php");
+require_once("include/settings.class.php");
 
 #Chargement des valeurs de la table settings
 $settings = new Settings();
 if (!$settings)
 	die("Erreur chargement settings");
 
-
 #Fonction relative à la session
-include_once "/include/session.inc.php";
+include_once "include/session.inc.php";
 
 // Resume session
 if (!grr_resumeSession()) {
@@ -38,6 +37,5 @@ if (isset($_GET['code_etablissement']) ) {
 	$_SESSION['current_etablisement']=  $_GET['code_etablissement'];
 }
 header("Location: ".htmlspecialchars_decode(page_accueil())."");
-
 
 ?>
