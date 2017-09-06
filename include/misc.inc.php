969c86c063f7d2719b31e6b6c826c737587867be
Christian <christian.daviau@recia.fr>
2017-09-06 17:39:43 +0200

    Tag de l'application en version 3.2

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -34,10 +34,10 @@
 $grr_devel_email = "support@devome.com";
 $grr_devel_url = "http://grr.devome.com/";
 // Numéro de version actuel
-$version_grr = "3.1.0";
+$version_grr = "3.2.0";
 // Numéro de sous-version actuel (a, b, ...)
 // Utilisez cette variable pour des versions qui corrigent la la version finale sans toucher à la base.
-$sous_version_grr = ""; // a, b, c, ...
+$sous_version_grr = "a"; // a, b, c, ...
 // Numéro de la release candidate (doit être strictement inférieure à 9). Laisser vide s'il s'agit de la version stable.
 $version_grr_RC = "";
 
